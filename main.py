5ce2eb254b73b0efa999391f41e09e8fce1bb3ba
Shanny Halamish <shannyhalamish@gmail.com>
2023-12-05 00:21:32 +0200

    function set time

--- main.py
+++ main.py
@@ -4,19 +4,56 @@
 
 class Clock():
 
-    def __init__(self, hours = 00, minutes = 0, seconds = 0):
+    def __init__(self, hours = 00, minutes = 00, seconds = 00):
 
-        self.hours = 00
-        self.minutes = 0
-        self.seconds = 0
+        self.hours = hours
+        self.minutes = minutes
+        self.seconds = seconds
 
-
+        self.time = str(self.hours) + ":" + str(self.minutes) + ":" + str(self.seconds)
 
     def get_time(self):
         '''
         take all the variables and join them into a sting - 00:00
         :return: hours : minutes seconds
         '''
-        time = str(self.hours) + ":" + str(self.minutes) + str(self.seconds)
-        return time
+        self.time = str(self.hours) + ":" + str(self.minutes) + ":" + str(self.seconds)
+        return
 
+
+
+    def set_time(self,value_type, new_value):
+        '''
+        will receive from the client what would he like to change(hours/minutes/seconds) and what value to change to
+        :param value_type: hours/minutes/seconds
+        :type new_value: int
+        :return: None
+        '''
+
+        #setting the new value
+        if value_type == "hours":
+            self.hours = new_value
+
+        elif value_type == "minutes":
+            self.minutes = new_value
+
+        elif value_type == "hours":
+            self.seconds = new_value
+        else:
+            print("please follow the given instractions...")
+
+        #changing the time
+
+       # get_time()
+
+        return
+
+    def add_time(self, value_type, new_value):
+        '''
+
+        :param value_type:
+        :param new_value:
+        :return:
+        '''
+
+        
